68462c1d3b178df522242feacbe6ba80a034c7aa
JoelyB <52165459+Joel-Barratt@users.noreply.github.com>
2019-12-09 15:43:45 -0500

    Add files via upload

--- Import_and_Trim.sh
+++ Import_and_Trim.sh
@@ -1,64 +1,118 @@
 #!/bin/bash
-
-###Haplotype_caller_pipeline - version - 1
-
-
-
-running_directory=/Users/joelbarratt/Documents/CYCLOSPORA/HAPLOTYPE_CALLER
-echo $running_directory > RUN_DIR
-
-junction_with_primer=$running_directory/MOCK_FOR_READ_RECOVERY_junction_with_primers.fasta ### this is for detection of new junction types - need to include the primers in this one
-echo $junction_with_primer > JUNC_REF
-
-complete_reference_database=$running_directory/mock.list.fasta ###trim the primers off the junction sequences for this one.
-echo $complete_reference_database > ALL_REF
-
-illumina_adapaters=$running_directory/Illumina_adapters.fasta
+#####################################################################################################################################################################
+#### IMPORT DATA AND TRIM RAW READS MODULE ##########################################################################################################################
+#####################################################################################################################################################################
 
 
+
+#####################################################################################################################################################################
+######                                                                         ######################################################################################
+######  SPECIFY LOCATION OF REFERENCE SEQUENCES AND RAW READS FOR ANALYSIS     ######################################################################################
+######                                                                         ######################################################################################
+#####################################################################################################################################################################
+HAPLOTYPE_CALLER=HAPLOTYPE_CALLER  # do not modify this line                   ######################################################################################
+#####################################################################################################################################################################   
+																
+
+
+                                            ##################################################### THE USER SHOULD MODIFY THESE LINES OF THE SCRIPT ONLY. PROVIDE THE 
+                                            ################################################### PIPELINE WITH THE LOCATION OF YOUR RAW READS AND REFERENCE SEQUENCES
+                                            ###
+                                            ###
+                                            ###
+                                         #########
+                                          #######
+                                           #####
+                                            ###
+                                             #
+
+# ONLY MODIFY TEXT TO THE RIGHT OF THE 'EQUALS' SIGN "=" BELOW - DO NOT MODIFY TEXT TO THE LEFT THE 'EQUAL' SIGN
+
+
+
+# IN WHAT DIRECTORY DID YOU PLACE THE "HAPLOTYPE_CALLER" FOLDER?
+
+working_directory=/Users/joelbarratt/Documents/CYCLOSPORA/
+
+
+
+# WHAT IS THE LOCATION OF YOUR ILLUMINA ADAPTER SEQUENCES FOR ADAPTER TRIMMING?
+# Modify this location if required, but I have already provided a list of defauly Illumina adapters that should work for most workflows.
+
+illumina_adapaters=$working_directory$HAPLOTYPE_CALLER/CYCLO_REF_SEQS/TRIMMING/Illumina_adapters.fasta                                           
+                                                                                                                                               
+
+                                                                                                                                             
+# WHAT IS THE LOCATION OF THE RAW ILLUMINA READS THAT YOU WANT ANALYZED?                                                                                   
+# R1 and R2 reads must be placed in the folder that you specify here. They will not be moved or modified.
+
+input_reads=$working_directory$HAPLOTYPE_CALLER/INPUT_READS
+                                                                  
+
+
+# WHAT IS THE LOCATION OF YOUR REFERENCE SEQUENCES (YOUR KNOWN CYCLOSPORA HAPLOTYPES)?
+# The location below should be fine for the automatic Cyclospora workflow - you probably shouldn't modify this.
+                            
+complete_reference_database=$working_directory$HAPLOTYPE_CALLER/CYCLO_REF_SEQS/BLASTING/ALL_ORIGINAL_REFS/2019_ORIGINAL_REFERENCES_SHORTENED.fasta
+
+
+
+########### DO NOT MODIFY SCRIPT BELOW THIS POINT.
+
+
+
+###########################################################################################################################################################
+######                                                  ###################################################################################################
+######  WRITE DIRECTORIES FOR TMP FILES & VARIABLES     ###################################################################################################
+######                                                  ###################################################################################################
+###########################################################################################################################################################
+
+LOC=$working_directory$HAPLOTYPE_CALLER
+
+mkdir TMP
+cd TMP
+
+echo $LOC/TMP > RUN_DIR
+echo $complete_reference_database/TMP > ALL_REF
+echo $LOC/TMP > LOC
+
+mkdir R1_FILES
+mkdir R2_FILES
+mkdir $LOC/RESULTS
 
 ##########################################################################################################################################################
 ######                                       #############################################################################################################
 ######  MOVE AND RENAME THE R1 AND R2 READS  #############################################################################################################
 ######                                       #############################################################################################################
 ##########################################################################################################################################################
+cd $input_reads
 
-cd $running_directory
-
-mkdir R1_FILES
-mkdir R2_FILES
-mkdir FINAL_GENOTYPING_RESULTS
-
-cd SPECIMENS_TO_GENOTYPE
-
-cp *_R1_* ../R1_FILES
-cp *_R2_* ../R2_FILES
+cp *_R1_* $LOC/TMP/R1_FILES
+cp *_R2_* $LOC/TMP/R2_FILES
 
 
-cd ..
-
-cd R1_FILES
+cd $LOC/TMP/R1_FILES
 ##RENAME THE FASTQ FILES IN R1 FOLDER
-	for file in *
-		do
-			mv "$file" `echo "$file" | sed -e 's/\(.\{11\}\).*.\(.\{3\}\)/\1.R1.fastq\2/'`
-		done
+   for file in *
+                do
+                     mv "$file" `echo "$file" | sed -e 's/\(.\{11\}\).*.\(.\{3\}\)/\1.R1.fastq\2/'`
+                done
 
 ##MAKE LIST OF SPECIMENS TO GENOTYPE
-	for file in *
-		do
-			echo "$file" | sed -e 's/^\(.\{11\}\).*/\1/' >> ../SPECIMENS_TO_RUN
-		done
+   for file in *
+                 do
+                      echo "$file" | sed -e 's/^\(.\{11\}\).*/\1/' >> $LOC/TMP/SPECIMENS_TO_TRIM   # MAKES A LIST OF SPECIMENS THAT NEED TRIMMING
+                 done
 
-cd ../R2_FILES
+cd $LOC/TMP/R2_FILES
 ##RENAME THE FASTQ FILES IN R2 FOLDER
-	for file in *
-		do
-			mv "$file" `echo "$file" | sed -e 's/\(.\{11\}\).*.\(.\{3\}\)/\1.R2.fastq\2/'`
+   for file in *
+                  do
+                       mv "$file" `echo "$file" | sed -e 's/\(.\{11\}\).*.\(.\{3\}\)/\1.R2.fastq\2/'`
 
-		done
+                  done
 
-cd $running_directory
+cd $LOC/TMP
 
 ##########################################################################################################################################################
 ######                               #####################################################################################################################
@@ -66,41 +120,42 @@ cd $running_directory
 ######                               #####################################################################################################################
 ##########################################################################################################################################################
 
-		for SPECIMEN_NAME in `cat SPECIMENS_TO_RUN`
-			do
-			#TRIM USING BBDUK
-
-bash $running_directory/BBMAP/bbmap/bbduk.sh in1=$running_directory/R1_FILES/$SPECIMEN_NAME.R1.fastq.gz in2=$running_directory/R2_FILES/$SPECIMEN_NAME.R2.fastq.gz out1=$running_directory/$SPECIMEN_NAME.clean1.fq out2=$running_directory/$SPECIMEN_NAME.clean2.fq minlen=50 ref=$illumina_adapaters ktrim=r k=23 mink=11 hdist=1
-
-			#MERGE USING BBMERGE
-
-bash $running_directory/BBMAP/bbmap/bbmerge-auto.sh in1=$running_directory/$SPECIMEN_NAME.clean1.fq in2=$running_directory/$SPECIMEN_NAME.clean2.fq out=$running_directory/$SPECIMEN_NAME.merged.fq outu=$running_directory/$SPECIMEN_NAME.unmerged.fq rem k=62 extend2=50 ecct
-
-			#NOTE FROM BBTOOLS MANUAL ADVIZING AGAINST QULITY TRIMMING PRIOR TO MERGING READS:
-			#Adapter-trimming reads is fine and is recommended prior to running BBDuk, particularly if kmers will be used. ***Quality-trimming is 
-			#usually not recommended*****, unless the library has major quality issues at the right end of reads resulting in a low merge rate, in which 
-			#case only weak trimming (such as qtrim=r trimq=8) should be used.
 
 
+         for SPECIMEN_NAME in `cat SPECIMENS_TO_TRIM`
+                                                            do
+
+# TRIM USING BBDUK
+
+bash $LOC/BBMAP/bbmap/bbduk.sh in1=$LOC/TMP/R1_FILES/$SPECIMEN_NAME.R1.fastq.gz in2=$LOC/TMP/R2_FILES/$SPECIMEN_NAME.R2.fastq.gz out1=$LOC/TMP/$SPECIMEN_NAME.clean1.fq out2=$LOC/TMP/$SPECIMEN_NAME.clean2.fq minlen=50 ref=$illumina_adapaters ktrim=r k=23 mink=11 hdist=1
+
+# MERGE USING BBMERGE
+
+bash $LOC/BBMAP/bbmap/bbmerge-auto.sh in1=$working_directory/$SPECIMEN_NAME.clean1.fq in2=$working_directory/$SPECIMEN_NAME.clean2.fq out=$working_directory/$SPECIMEN_NAME.merged.fq outu=$working_directory/$SPECIMEN_NAME.unmerged.fq rem k=62 extend2=50 ecct
 
 
-						#CONCATENATE MERGED AND UNMERGED READS AND PLACE IN A 'PROCESSED_READS' DIRECTORY FOR DOWNSTREAM USE
-
-						mkdir $running_directory/PROCESSED_READS
-
-						mv $running_directory/$SPECIMEN_NAME.merged.fq $running_directory/PROCESSED_READS/$SPECIMEN_NAME.clean_merged.fastq
-
-						cat $running_directory/$SPECIMEN_NAME.unmerged.fq >> $running_directory/PROCESSED_READS/$SPECIMEN_NAME.clean_merged.fastq
-						rm -rf $SPECIMEN_NAME.clean1.fq $SPECIMEN_NAME.clean2.fq $SPECIMEN_NAME.unmerged.fq
-
-			done
-
-
-#TIDY UP BY REMOVING FILES NO LONGER NEEDED
-rm -rf R2_FILES R1_FILES SPECIMENS_TO_RUN
-
-bash Finding_New_Junction_Types_Module.sh
+ # NOTE FROM BBTOOLS MANUAL ADVIZING AGAINST QULITY TRIMMING PRIOR TO MERGING READS:
+ # Adapter-trimming reads is fine and is recommended prior to running BBDuk, particularly if kmers will be used. ***Quality-trimming is 
+ # usually not recommended*****, unless the library has major quality issues at the right end of reads resulting in a low merge rate, in which 
+ # case only weak trimming (such as qtrim=r trimq=8) should be used.
 
 
 
+
+                                                # CONCATENATE MERGED AND UNMERGED READS AND PLACE IN A 'PROCESSED_READS' DIRECTORY FOR DOWNSTREAM USE
+
+                                                mkdir $LOC/TMP/PROCESSED_READS
+                                                mv $LOC/TMP/$SPECIMEN_NAME.merged.fq $LOC/TMP/PROCESSED_READS/$SPECIMEN_NAME.clean_merged.fastq
+                                                cat $LOC/TMP/$SPECIMEN_NAME.unmerged.fq >> $LOC/TMP/PROCESSED_READS/$SPECIMEN_NAME.clean_merged.fastq
+                                                rm -rf $SPECIMEN_NAME.clean1.fq $SPECIMEN_NAME.clean2.fq $SPECIMEN_NAME.unmerged.fq
+ 
+
+                                                           done
+
+
+#bash Finding_New_Junction_Types_Module.sh
+
+#bash Finding_New_Haps_SNP_Based_Module.sh
+
+#bash Call_Haplotypes.sh
 
