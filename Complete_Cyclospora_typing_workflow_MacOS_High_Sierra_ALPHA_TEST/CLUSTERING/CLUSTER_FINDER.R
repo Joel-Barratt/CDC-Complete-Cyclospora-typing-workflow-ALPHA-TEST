5603936f74f95a1e2d300f576b934f4a168a6c97
JoelyB <52165459+Joel-Barratt@users.noreply.github.com>
2021-08-05 13:42:32 -0400

    Update CLUSTER_FINDER.R

--- Complete_Cyclospora_typing_workflow_MacOS_High_Sierra_ALPHA_TEST/CLUSTERING/CLUSTER_FINDER.R
+++ Complete_Cyclospora_typing_workflow_MacOS_High_Sierra_ALPHA_TEST/CLUSTERING/CLUSTER_FINDER.R
@@ -10,7 +10,7 @@ library(spaa)
 require(parallel)
 require(stringr)
 
-#UP November 19, 2020
+#UPDATED August 5, 2021
 
 date_today <- Sys.Date()
 
@@ -19,29 +19,22 @@ temporary_directory <- getwd()
 
 
 
-#### First you need to import the most recent ensemble matrix as a variable in R called "matrix" in R
 
 matrix_location <- readLines("MATRIX_LOCATION")
-
 setwd(matrix_location)
-
 myFiles <- list.files(pattern="*csv", all.files = FALSE)
 myMatrix <- (sort(myFiles, decreasing = TRUE)[1])
-
 matrix <- as.matrix(read.table(myMatrix, sep = ",", row.names=1, header=TRUE))
 
 
 setwd(temporary_directory)
 
-##### Now you need to calculate the number of genetic clusters using the "gold standard" list. Use this to calculate the number of genetic clusters. "number_of_genetic_clusters" needs to be the name of this variable.
 
 import_gold_standards <- readLines("GOLD_CLUSTERS")
-cluster_standards <- read.table(import_gold_standards, head=TRUE)  #THIS IS WHERE YOU DEFINE WHICH Seq_IDs are your gold standard "within cluster" distances
+cluster_standards <- read.table(import_gold_standards, head=TRUE)
 
-###set range of cluster numbers to test
 min_cluster_number = as.numeric(readLines("CLUSTER_MIN"))
 max_cluster_number = as.numeric(readLines("CLUSTER_MAX"))
-
 
 
 source("../cluster_counter.R")
