053742f9121264c097361eb4bdf95b70bbaa8a0e
JoelyB <52165459+Joel-Barratt@users.noreply.github.com>
2021-08-05 13:40:41 -0400

    tidy cluster counter

--- Complete_Cyclospora_typing_workflow_MacOS_High_Sierra_ALPHA_TEST/CLUSTERING/cluster_counter.R
+++ Complete_Cyclospora_typing_workflow_MacOS_High_Sierra_ALPHA_TEST/CLUSTERING/cluster_counter.R
@@ -1,44 +1,32 @@
-#UP November 19, 2020
+#UPDATED August 5, 2021
 
 stringency_level = as.numeric(readLines("STRINGENCY"))
 number_of_threads = as.numeric(readLines("THREADS"))
 
 
 
-###### CALCULATE THE GOLDEN DISTANCE CUTOFF FOR EACH CLUSTER
 gold_standard_clusters_A <- cluster_standards
-
 colnames(gold_standard_clusters_A) <- c("Seq_ID", "Standard_number_A")
 gold_standard_clusters_B <- gold_standard_clusters_A
-#head(gold_standard_clusters_A, 5)
 colnames(gold_standard_clusters_B) <- c("row", "Standard_number_B")
-#head(gold_standard_clusters_B, 5)
 list <- dist2list(as.dist(matrix))
 attach(list)
-sorted_list <- list[order(value),] #### sorts by the distance in the list, smallest to largest.
+sorted_list <- list[order(value),] 
 sorted_list <- as.data.frame(sorted_list)
-#sorted_list_filtered <- sorted_list %>% filter(value < 0.7) ###THIS WILL FILTER VALUES BELOW 0.7 BASED ON THE COLUMN CALLED "VALUE".
 
 
 colnames(sorted_list) = NULL
 colnames(sorted_list) <- c("Seq_ID", "row", "distance")
-#head(sorted_list, 5)
 total <-merge(sorted_list, gold_standard_clusters_A, by=c("Seq_ID"), all.x=TRUE)
-#head(total, 5)
 total_final <- merge(total, gold_standard_clusters_B, by=c("row"), all.x=TRUE)
-#head(total_final, 5)
 last_column <- paste0(total_final$Standard_number_A,"plus",total_final$Standard_number_B,"_")
-#head(last_column, 10)
 set_gold_standard <- cbind(total_final, last_column)
-#head(set_gold_standard,10)
 
 
 
 
 cluster_to_cluster_A <- set_gold_standard %>% filter(str_detect(Standard_number_A, "NA") == FALSE)
-#head(cluster_to_cluster_A,10)
 cluster_to_cluster_A_and_B <- cluster_to_cluster_A %>% filter(str_detect(Standard_number_B, "NA") == FALSE)
-#head(cluster_to_cluster_A_and_B,10)
 
 
 within_cluster_standard_distance <- NULL
@@ -56,20 +44,10 @@ within_cluster_standard_distance <- rbind(within_cluster_standard_distance, stan
 }
 
 
-######THE TWO LINES BELOW IF WE WANT SPECIFICALLY THE MCDONALDS AND KT CLUSTERS FROM 2018 TO BE THE STANDARD
 
 golden_cluster_distance <- mean(within_cluster_standard_distance$distance) + 3*(sd(within_cluster_standard_distance$distance))
-
-
-############################################# IDENTIFY THE CORRECT CLUSTER NUMBER
-#### THIS WILL BE THE SMALLEST NUMBER OF CLUSTERS, THAT HAVE EVERY WITHIN-CLUSTER DISTANCE FALLING BELOW THE GOLDEN DISTANCE
-
 date_today <- Sys.Date()
 
-
-
-
-## can we make this run in parallel
 
 Ensemble <- as.matrix(matrix)
 Ensemble_x <- as.hclust(agnes(x=Ensemble, diss = TRUE, stand = TRUE, metric = "manhattan", method = "ward"))
@@ -77,14 +55,12 @@ Ensemble_x <- as.hclust(agnes(x=Ensemble, diss = TRUE, stand = TRUE, metric = "m
 
 check_all_cluster_numbers = mclapply(min_cluster_number:max_cluster_number, function (f) {
 
-#name_clus <- paste(date_today,"_there_are_",f,"_genetic_clusters.txt", sep="")
 cluster <- as.data.frame(melt(factor(cutree(Ensemble_x, k=f))))
 colnames(cluster) <- NULL
 cluster <- cbind(rownames(cluster), cluster)
 colnames(cluster) <- c("Seq_ID", "clus_A")
 
 
-#### THIS IS WHAT THE WHOLE PAIRED DISTANCE TABLE IS CALLED :sorted_list
 cluster_first_col <-merge(sorted_list, cluster, by=c("Seq_ID"), all.x=TRUE)
 colnames(cluster) <- NULL
 colnames(cluster) <- c("row", "clus_B")
@@ -93,7 +69,7 @@ cluster_calc <- subset(cluster_second_col, clus_A == clus_B)
 
 distances_below_gold_cluster_distance <- cluster_calc %>% filter(distance < golden_cluster_distance)
 
-percent_within_cluster_distances_meeting_cutoff <- ((length(distances_below_gold_cluster_distance$distance))/length(cluster_calc$distance))*100 #this final result will be assigned as the output of the function.
+percent_within_cluster_distances_meeting_cutoff <- ((length(distances_below_gold_cluster_distance$distance))/length(cluster_calc$distance))*100 # this will be assigned as the output of the function.
 
 }, mc.cores= number_of_threads)
 
